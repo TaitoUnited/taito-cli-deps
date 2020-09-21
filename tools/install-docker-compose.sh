a2d81ff7f1dd4ff6bf8250989cc2e72d46820f74
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2020-09-22 02:42:02 +0300

    feat: update deps

--- tools/install-docker-compose.sh
+++ tools/install-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-1.26.2} && \
+    export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-1.27.3} && \
     curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
--- tools/install-helm.sh
+++ tools/install-helm.sh
@@ -4,8 +4,8 @@ set -eux; \
     curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get \
       > get_helm.sh && \
     chmod 700 get_helm.sh && \
-    ./get_helm.sh -v v2.16.10 && \
+    ./get_helm.sh -v v2.16.12 && \
     mv /usr/local/bin/helm /usr/local/bin/helm2 && \
-    export HELM_VERSION=${HELM_VERSION:-v3.3.1} && \
+    export HELM_VERSION=${HELM_VERSION:-v3.3.3} && \
     ./get_helm.sh -v "${HELM_VERSION}" && \
     rm ./get_helm.sh
