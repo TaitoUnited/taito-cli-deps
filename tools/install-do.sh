cccfb81ced7d5f170f204a9ccbd316e350911f00
keskiju <jukka.keskiluopa@taitounited.fi>
2021-04-29 23:35:36 +0300

    feat: update deps

--- tools/install-do.sh
+++ tools/install-do.sh
@@ -2,6 +2,6 @@
 
 set -eux; \
     cd /tmp && \
-    export DO_VERSION=${DO_VERSION:-1.58.0} && \
+    export DO_VERSION=${DO_VERSION:-1.59.0} && \
     curl -sL https://github.com/digitalocean/doctl/releases/download/v${DO_VERSION}/doctl-${DO_VERSION}-linux-amd64.tar.gz | tar -xzv && \
     mv doctl /usr/local/bin/doctl
