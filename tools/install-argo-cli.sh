4258a0a09de74bf585911beae010007d8d77aed8
keskiju <jukka.keskiluopa@taitounited.fi>
2021-05-01 19:26:51 +0300

    feat: add argo crds

--- tools/install-argo-cli.sh
+++ tools/install-argo-cli.sh
@@ -7,4 +7,13 @@ set -eux; \
   gunzip argo-linux-amd64.gz && \
   chmod +x argo-linux-amd64 && \
   mv ./argo-linux-amd64 /usr/local/bin/argo && \
-  argo version
+  argo version && \
+  mkdir -p /tools/argo/crds && \
+  cd /tools/argo/crds && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/README.md && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_clusterworkflowtemplates.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_cronworkflows.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_workfloweventbindings.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_workflows.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_workflowtemplates.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/kustomization.yaml
