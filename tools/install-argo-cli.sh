4222282962646a5c8ec8d661a9e2f8715b6c8925
keskiju <jukka.keskiluopa@taitounited.fi>
2021-05-01 23:10:49 +0300

    feat: add argo-events crds

--- tools/install-argo-cli.sh
+++ tools/install-argo-cli.sh
@@ -8,12 +8,18 @@ set -eux; \
   chmod +x argo-linux-amd64 && \
   mv ./argo-linux-amd64 /usr/local/bin/argo && \
   argo version && \
-  mkdir -p /tools/argo/crds && \
-  cd /tools/argo/crds && \
+  mkdir -p /tools/argo-workflows/crds && \
+  cd /tools/argo-workflows/crds && \
   curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/README.md && \
   curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_clusterworkflowtemplates.yaml && \
   curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_cronworkflows.yaml && \
   curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_workfloweventbindings.yaml && \
   curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_workflows.yaml && \
   curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/argoproj.io_workflowtemplates.yaml && \
-  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/kustomization.yaml
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-workflows/master/manifests/base/crds/minimal/kustomization.yaml && \
+  mkdir -p /tools/argo-events/crds && \
+  cd /tools/argo-events/crds && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-events/master/manifests/base/crds/argoproj.io_eventbus.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-events/master/manifests/base/crds/argoproj.io_eventsources.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-events/master/manifests/base/crds/argoproj.io_sensors.yaml && \
+  curl -O -J https://raw.githubusercontent.com/argoproj/argo-events/master/manifests/base/crds/kustomization.yaml
