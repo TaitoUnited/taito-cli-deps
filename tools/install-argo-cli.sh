d4ebe0c4e7f897ae97b4d828f8ab675e900b67fc
keskiju <jukka.keskiluopa@taitounited.fi>
2021-05-26 23:15:46 +0300

    feat: update deps

--- tools/install-argo-cli.sh
+++ tools/install-argo-cli.sh
@@ -2,7 +2,7 @@
 
 set -eux; \
   cd /tmp && \
-  export ARGO_VERSION=${ARGO_VERSION:-3.0.2} && \
+  export ARGO_VERSION=${ARGO_VERSION:-3.0.7} && \
   curl -sLO https://github.com/argoproj/argo/releases/download/v${ARGO_VERSION}/argo-linux-amd64.gz && \
   gunzip argo-linux-amd64.gz && \
   chmod +x argo-linux-amd64 && \
