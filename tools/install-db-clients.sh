34495d38a46632344c1ecdc630c2face7708bf36
keskiju <jukka.keskiluopa@taitounited.fi>
2021-06-05 22:14:55 +0300

    feat: install postgresql-client-12

--- tools/install-db-clients.sh
+++ tools/install-db-clients.sh
@@ -2,6 +2,8 @@
 
 set -eux; \
     apt-get -qqy update && \
-    apt-get -qqy install postgresql-client default-mysql-client && \
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add - && \
+    echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list && \
+    apt-get -qqy install postgresql-client-12 default-mysql-client && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
