0ea5b3ee33279eb38c283ecf431b1741ee70fae2
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-06-21 15:38:36 +0300

    feat: install aws-sdk

--- tools/install-aws.sh
+++ tools/install-aws.sh
@@ -5,4 +5,5 @@ set -eux; \
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && \
     unzip awscli-bundle.zip && \
     ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws && \
-    rm -rf awscli*
+    rm -rf awscli* && \
+    npm install -g aws-cdk
