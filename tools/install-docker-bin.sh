850521dacf2b11c44e2e86e5c70d6b370ba88c28
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-06-21 15:38:24 +0300

    feat: update deps

--- tools/install-docker-bin.sh
+++ tools/install-docker-bin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    export DOCKER_VERSION=${DOCKER_VERSION:-20.10.6} && \
+    export DOCKER_VERSION=${DOCKER_VERSION:-20.10.7} && \
     curl -fsSL \
       https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_VERSION.tgz | \
       tar -xzvf - docker/docker && \
