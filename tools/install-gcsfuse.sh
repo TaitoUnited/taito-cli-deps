7c05b8632f1a02cae6178358df17311acc8d4876
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-19 02:36:04 +0300

    chore: update gcsfuse installation script

--- tools/install-gcsfuse.sh
+++ tools/install-gcsfuse.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    echo "deb http://packages.cloud.google.com/apt gcsfuse-stretch main" | \
-      tee /etc/apt/sources.list.d/gcsfuse.list; \
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+    export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s` && \
+    echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | && \
+      tee /etc/apt/sources.list.d/gcsfuse.list && \
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |
       apt-key add - && \
     apt-get -y update && \
     apt-get -y install gcsfuse && \
