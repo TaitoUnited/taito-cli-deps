cd3cc85f763b3395d4f5899330b314d3e337dcdf
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-19 09:18:34 +0300

    fix: docker-compose binary not available for linux/arm64

--- tools/install-docker-compose.sh
+++ tools/install-docker-compose.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
+# TODO: use alternative installation for linux/arm64
+if [ ${TARGETPLATFORM} != "linux/arm64" ]; then
 set -eux; \
     export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-1.29.2} && \
     curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
+fi
