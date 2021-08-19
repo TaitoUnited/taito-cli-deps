9c405897e363129acae5b4cd9f695d0708cb2922
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-19 08:58:26 +0300

    fix: use aarch64 for linux/arm64

--- tools/install-docker-bin.sh
+++ tools/install-docker-bin.sh
@@ -2,8 +2,10 @@
 
 set -eux; \
     export DOCKER_VERSION=${DOCKER_VERSION:-20.10.8} && \
+    export DOCKER_ARCH="x86_64" && \
+    if [ ${TARGETPLATFORM} = "linux/arm64" ]; then export DOCKER_ARCH="aarch64"; fi && \
     curl -fsSL \
-      https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_VERSION.tgz | \
+      https://download.docker.com/linux/static/stable/${DOCKER_ARCH}/docker-$DOCKER_VERSION.tgz | \
       tar -xzvf - docker/docker && \
     upx -9v docker/docker && \
     mv docker/docker /usr/bin/docker && \
--- tools/install-dockerd-bin.sh
+++ tools/install-dockerd-bin.sh
@@ -4,8 +4,10 @@ set -eux; \
     apt-get -qqy update && \
     apt-get -qqy install iptables && \
     export DOCKER_VERSION=${DOCKER_VERSION:-20.10.8} && \
+    export DOCKER_ARCH="x86_64" && \
+    if [ ${TARGETPLATFORM} = "linux/arm64" ]; then export DOCKER_ARCH="aarch64"; fi && \
     curl -fsSL \
-      https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_VERSION.tgz | \
+      https://download.docker.com/linux/static/stable/${DOCKER_ARCH}/docker-$DOCKER_VERSION.tgz | \
       tar -xzvf - docker && \
     rm docker/docker && \
     upx -9v docker/* && \
