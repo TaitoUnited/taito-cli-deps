5d3c647a3b05d1f2f9aab7c9c08cb63d6629fe86
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-19 23:33:38 +0300

    fix(gcloud): install cloud_sql_proxy

--- tools/install-gcloud.sh
+++ tools/install-gcloud.sh
@@ -5,4 +5,8 @@ set -eux; \
       tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |
       apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && \
-    apt-get update -y && apt-get install google-cloud-sdk -y
+    apt-get update -y && apt-get install google-cloud-sdk -y && \
+    curl "https://dl.google.com/cloudsql/cloud_sql_proxy.linux.${TARGETPLATFORM#linux/}" \
+       > cloud_sql_proxy && \
+     chmod +x cloud_sql_proxy && \
+     mv cloud_sql_proxy /usr/local/bin
