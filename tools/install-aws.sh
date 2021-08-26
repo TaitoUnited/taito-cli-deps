3f8ffe4e374581996c536d37cd110579d7a9ccd8
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-26 16:52:57 +0300

    feat: install python3-venv only for aws-cli

--- tools/install-aws.sh
+++ tools/install-aws.sh
@@ -2,8 +2,14 @@
 
 set -eux; \
     cd /tmp && \
+    apt-get -qqy update && \
+    apt-get -y install python3-venv && \
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && \
     unzip awscli-bundle.zip && \
     ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws && \
     rm -rf awscli* && \
-    npm install -g aws-cdk
+    npm install -g aws-cdk && \
+    apt-get -qqy --purge remove python3-venv && \
+    apt-get -qqy --purge autoremove && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
--- tools/install-python3.sh
+++ tools/install-python3.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
+# NOTE: required for full python programming env:
+# build-essential libssl-dev libffi-dev python3-dev python3-pip
+
 set -eux; \
     apt-get -qqy update && \
-    apt-get -qqy install python3 python3-venv && \
+    apt-get -qqy install python3 && \
     rm -rf /usr/bin/python && \
     ln -s /usr/bin/python3.7 /usr/bin/python && \
     python --version && \
