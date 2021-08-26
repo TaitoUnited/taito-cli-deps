d25dd1239893e33c46d665f9fc2c4af7c3846ceb
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-26 22:50:09 +0300

    feat: remove git from cli

--- tools/install-bats.sh
+++ tools/install-bats.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
 set -eux; \
+    apt-get -y update && \
+    apt-get -y install git && \
+
     git clone https://github.com/bats-core/bats-core.git && \
     ./bats-core/install.sh /usr/local && \
-    rm -rf bats-core
+    rm -rf bats-core && \
+
+    apt-get -qqy --purge remove git && \
+    apt-get -qqy --purge autoremove && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
