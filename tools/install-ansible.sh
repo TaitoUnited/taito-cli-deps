dee5475600c17713c0291bb96836430fed1832ef
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-26 12:49:57 +0300

    fix: install missing ansible python modules

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -5,7 +5,10 @@ set -eux; \
       >> /etc/apt/sources.list && \
     apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 && \
     (apt-get -y update || :) && \
-    apt-get -y install ansible && \
+    # TODO: python3-pip and pip3 install ansible should not be required
+    apt-get -y install ansible python3-pip && \
+    pip3 install ansible && \
+    apt-get -qqy --purge remove python3-pip && \
     apt-get -qqy --purge autoremove && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
