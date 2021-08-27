ced05a604319dff1daa23da3c9d2fa1c35b717af
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-08-27 10:39:38 +0300

    fix: remove helm2 from user init

--- tools/user-init.sh
+++ tools/user-init.sh
@@ -14,18 +14,6 @@ echo "Initializing user $username"
 echo "-----------------------------------------------------------------"
 echo
 
-# Helm v2
-${su} /bin/sh -c "
-  helm2 init --client-only &&
-  helm2 plugin install https://github.com/rimusz/helm-tiller &&
-  helm2 tiller start-ci &&
-  helm2 repo add taito-charts https://taitounited.github.io/taito-charts/ &&
-  helm2 repo add jetstack https://charts.jetstack.io &&
-  helm2 repo add bitnami https://charts.bitnami.com/bitnami &&
-  helm2 repo update &&
-  helm2 tiller stop
-"
-
 # Helm v3
 ${su} /bin/sh -c "
   helm repo add taito-charts https://taitounited.github.io/taito-charts &&
