1e0de52956e442abcee591602762d91b8eeb1b01
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-09-19 16:02:19 +0300

    fix(aws): install aws cli with python3

--- tools/install-aws.sh
+++ tools/install-aws.sh
@@ -8,7 +8,7 @@ set -eux; \
     apt-get -y install unzip python3-venv && \
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" && \
     unzip awscli-bundle.zip && \
-    ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws && \
+    python3 awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws && \
     rm -rf awscli* && \
     npm install -g aws-cdk && \
     apt-get -qqy --purge remove unzip python3-venv && \
