a4a52a4a7c2b48dbc19268b428c9d0a57643794c
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-09-19 16:04:14 +0300

    feat: keep python2 as default for now

--- tools/install-python3.sh
+++ tools/install-python3.sh
@@ -6,9 +6,6 @@
 set -eux; \
     apt-get -qqy update && \
     apt-get -qqy install python3 && \
-    rm -rf /usr/bin/python && \
-    ln -s /usr/bin/python3.7 /usr/bin/python && \
-    python --version && \
     apt-get -qqy --purge autoremove && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
