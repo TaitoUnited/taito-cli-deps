e08f30fa0551568c046055461ea480ecdfc77a9a
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-11-16 02:24:49 +0200

    chore: update terraform and helm

--- tools/install-do.sh
+++ tools/install-do.sh
@@ -2,6 +2,6 @@
 
 set -eux; \
     cd /tmp && \
-    export DO_VERSION=${DO_VERSION:-1.65.0} && \
+    export DO_VERSION=${DO_VERSION:-1.67.0} && \
     curl -sL "https://github.com/digitalocean/doctl/releases/download/v${DO_VERSION}/doctl-${DO_VERSION}-linux-${TARGETPLATFORM#linux/}.tar.gz" | tar -xzv && \
     mv doctl /usr/local/bin/doctl
