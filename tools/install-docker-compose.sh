e6124696679e4de0e249c399eadab9ba3b044eb5
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-11-16 02:24:36 +0200

    fix: add docker-compose on path for now

--- tools/install-docker-compose.sh
+++ tools/install-docker-compose.sh
@@ -6,5 +6,7 @@ set -eux; \
     mkdir -p /usr/local/lib/docker/cli-plugins && \
     export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-2.1.1} && \
     curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/lib/docker/cli-plugins/docker-compose && \
-    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose && \
+    # TODO: remove ->
+    cp /usr/local/lib/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
 fi
