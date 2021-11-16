3d4fcea059883b8cc390ede7c0f4b094c7dd49ad
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-11-16 09:23:15 +0200

    fix: revert back to docker-compose v1

--- tools/install-docker-compose.sh
+++ tools/install-docker-compose.sh
@@ -3,10 +3,7 @@
 # TODO: use alternative installation for linux/arm64
 if [ ${TARGETPLATFORM} != "linux/arm64" ]; then
 set -eux; \
-    mkdir -p /usr/local/lib/docker/cli-plugins && \
-    export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-2.1.1} && \
-    curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/lib/docker/cli-plugins/docker-compose && \
-    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose && \
-    # TODO: remove ->
-    cp /usr/local/lib/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
+    export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-1.29.2} && \
+    curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+    chmod +x /usr/local/bin/docker-compose
 fi
