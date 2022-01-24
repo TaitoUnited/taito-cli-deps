b2235e0eaf74450057aa2c229b95c8103b6ee5c2
keskiju <jukka.keskiluopa@taitounited.fi>
2022-01-24 13:19:26 +0200

    feat: update deps

--- tools/install-helm.sh
+++ tools/install-helm.sh
@@ -4,6 +4,6 @@ set -eux; \
     curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get \
       > get_helm.sh && \
     chmod 700 get_helm.sh && \
-    export HELM_VERSION=${HELM_VERSION:-v3.7.1} && \
+    export HELM_VERSION=${HELM_VERSION:-v3.7.2} && \
     ./get_helm.sh -v "${HELM_VERSION}" && \
     rm ./get_helm.sh
--- tools/install-terraform.sh
+++ tools/install-terraform.sh
@@ -6,7 +6,7 @@ set -eux; \
 
     mkdir /terraform && \
     cd /terraform && \
-    export TERRAFORM_VERSION=${TERRAFORM_VERSION:-1.0.11} && \
+    export TERRAFORM_VERSION=${TERRAFORM_VERSION:-1.1.4} && \
     curl -o terraform.zip "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_${TARGETPLATFORM#linux/}.zip" && \
     unzip terraform.zip && \
     mv terraform /usr/local/bin/terraform && \
