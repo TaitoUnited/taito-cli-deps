b560ff0d5680fe3a7c0ff64d781a1023031b5356
keskiju <jukka.keskiluopa@taitounited.fi>
2022-06-15 00:18:41 +0300

    chore: kubectl 1.23

--- tools/install-kubectl.sh
+++ tools/install-kubectl.sh
@@ -4,7 +4,8 @@ set -eux; \
     curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg && \
     echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list && \
     apt-get -qqy update && \
-    apt-get -qqy install kubectl && \
+    export KUBECTL_VERSION=${KUBECTL_VERSION:-1.23.7-00} && \
+    apt-get -qqy install "kubectl=${KUBECTL_VERSION}" && \
     apt-get -qqy --purge autoremove && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
