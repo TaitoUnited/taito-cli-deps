da10f5889ebb97668db59966c27d1e3adabc2909
keskiju <jukka.keskiluopa@taitounited.fi>
2022-11-22 00:10:57 +0200

    fix: k9s arch

--- tools/install-k9s.sh
+++ tools/install-k9s.sh
@@ -3,7 +3,8 @@
 set -eux; \
     cd /tmp && \
     export K9S_VERSION=${K9S_VERSION:-0.26.7} && \
-    export K9S_ARCH="Linux_${TARGETPLATFORM#linux/}" && \
+    export K9S_ARCH="Linux_x86_64" && \
+    if [ ${TARGETPLATFORM} = "linux/arm64" ]; then export K9S_ARCH="Linux_arm64"; fi && \
     curl -L "https://github.com/derailed/k9s/releases/download/v${K9S_VERSION}/k9s_${K9S_ARCH}.tar.gz" | tar -xz && \
     mv k9s /usr/bin && \
     rm -rf *
