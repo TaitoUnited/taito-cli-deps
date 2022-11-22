480a1ca2fc52014cc15a3c9d7a7467e44dbbb4cd
keskiju <jukka.keskiluopa@taitounited.fi>
2022-11-22 12:32:46 +0200

    fix: docker-compose installation

--- tools/install-docker-compose.sh
+++ tools/install-docker-compose.sh
@@ -2,5 +2,5 @@
 
 set -eux; \
     export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-2.12.2} && \
-    curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+    curl -L "https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
