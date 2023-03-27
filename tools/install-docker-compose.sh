757f2b7b091c1d471d49a6202ebbd832c59fc767
keskiju <jukka.keskiluopa@taitounited.fi>
2023-03-27 23:58:55 +0300

    chore: update versions

--- tools/install-docker-compose.sh
+++ tools/install-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-2.15.1} && \
+    export DOCKER_COMPOSE_VERSION=${DOCKER_COMPOSE_VERSION:-2.17.2} && \
     curl -L "https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
     chmod +x /usr/local/bin/docker-compose
