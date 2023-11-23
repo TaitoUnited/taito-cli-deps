e7e463b6dd144a15830006ae901cae85d77bc2e1
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-23 20:10:03 +0200

    fix: do not pack with upx-ucl for now

--- tools/install-docker-bin.sh
+++ tools/install-docker-bin.sh
@@ -2,7 +2,7 @@
 
 set -eux; \
     apt-get -y update && \
-    apt-get -y install upx-ucl && \
+    # apt-get -y install upx-ucl && \
 
     export DOCKER_VERSION=${DOCKER_VERSION:-24.0.7} && \
     export DOCKER_ARCH="x86_64" && \
@@ -10,12 +10,12 @@ set -eux; \
     curl -fsSL \
       https://download.docker.com/linux/static/stable/${DOCKER_ARCH}/docker-$DOCKER_VERSION.tgz | \
       tar -xzvf - docker/docker && \
-    upx -9v docker/docker && \
+    # upx -9v docker/docker && \
     mv docker/docker /usr/bin/docker && \
     rmdir docker && \
     groupadd --gid 999 docker && \
 
-    apt-get -qqy --purge remove upx-ucl && \
+    # apt-get -qqy --purge remove upx-ucl && \
     apt-get -qqy --purge autoremove && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
