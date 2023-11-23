56c6256485fb87dc5fa2d1720d33d17828aa5f4a
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-23 19:47:55 +0200

    chore: use debian bullseye for now (upx-ucl was removed)

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    export UBUNTU_VERSION=jammy && \
+    export UBUNTU_VERSION=focal && \
     curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg && \
     echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_VERSION main" | tee /etc/apt/sources.list.d/ansible.list && \
     (apt-get -y update || :) && \
