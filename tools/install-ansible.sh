d2c80ef3c0de4e33965e110113c4924e7d08b591
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-23 19:29:39 +0200

    fix: install-ansible script with curl instead of wget

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -2,7 +2,7 @@
 
 set -eux; \
     export UBUNTU_VERSION=jammy && \
-    wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg && \
+    curl -fsSL "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg && \
     echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_VERSION main" | tee /etc/apt/sources.list.d/ansible.list && \
     (apt-get -y update || :) && \
     apt-get -y install ansible sshpass && \
