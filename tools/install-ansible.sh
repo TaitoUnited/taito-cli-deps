1b8d691394df43e913affb368dcdbb48913a2ffc
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-23 19:14:18 +0200

    fix: install-ansible script

--- tools/install-ansible.sh
+++ tools/install-ansible.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" \
-      >> /etc/apt/sources.list && \
-    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 && \
+    export UBUNTU_VERSION=jammy && \
+    wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg && \
+    echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_VERSION main" | tee /etc/apt/sources.list.d/ansible.list && \
     (apt-get -y update || :) && \
-    # TODO: python3-pip and pip3 install ansible should not be required
-    apt-get -y install ansible python3-pip sshpass && \
-    pip3 install ansible && \
-    apt-get -qqy --purge remove python3-pip && \
+    apt-get -y install ansible sshpass && \
     apt-get -qqy --purge autoremove && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
