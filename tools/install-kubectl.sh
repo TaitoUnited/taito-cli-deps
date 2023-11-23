aa345f438e3d05cdd6033141f16255ab31335dea
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-23 18:31:26 +0200

    fix: kubectl install script

--- tools/install-kubectl.sh
+++ tools/install-kubectl.sh
@@ -3,8 +3,8 @@
 set -eux; \
     export KUBECTL_BASE_VERSION=${KUBECTL_VERSION:-1.27} && \
     export KUBECTL_VERSION=${KUBECTL_VERSION:-1.27.6-00} && \
-    curl -fsSL https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
-    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
+    echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list && \
     apt-get -qqy update && \
     apt-get -qqy install "kubectl=${KUBECTL_VERSION}" && \
     apt-get -qqy --purge autoremove && \
