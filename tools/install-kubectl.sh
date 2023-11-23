3342faa5b845b149815f8b3868a91871c00edc54
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-23 18:49:52 +0200

    fix: kubectl install script

--- tools/install-kubectl.sh
+++ tools/install-kubectl.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    export KUBECTL_BASE_VERSION=${KUBECTL_VERSION:-1.27} && \
-    export KUBECTL_VERSION=${KUBECTL_VERSION:-1.27.6-00} && \
+    export KUBECTL_BASE_VERSION=${KUBECTL_BASE_VERSION:-1.27} && \
     curl -fsSL https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
     echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list && \
     apt-get -qqy update && \
-    apt-get -qqy install "kubectl=${KUBECTL_VERSION}" && \
+    apt-get -qqy install kubectl && \
     apt-get -qqy --purge autoremove && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
