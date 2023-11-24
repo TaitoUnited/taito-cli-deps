91e23e9d64e8918c1edec22c08eb77d44e978309
keskiju <jukka.keskiluopa@taitounited.fi>
2023-11-24 14:46:42 +0200

    Revert "fix: revert to older docker version (depends_on change brakes ci/cd tests?)"
    This reverts commit 6af70bbab3f42f328ba97816b6ce920496c49951.

--- tools/install-dockerd-bin.sh
+++ tools/install-dockerd-bin.sh
@@ -4,7 +4,7 @@ set -eux; \
     apt-get -qqy update && \
     apt-get -qqy install iptables && \
 
-    export DOCKER_VERSION=${DOCKER_VERSION:-23.0.1} && \
+    export DOCKER_VERSION=${DOCKER_VERSION:-24.0.7} && \
     export DOCKER_ARCH="x86_64" && \
     if [ ${TARGETPLATFORM} = "linux/arm64" ]; then export DOCKER_ARCH="aarch64"; fi && \
     curl -fsSL \
