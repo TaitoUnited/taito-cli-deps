84f01f558f807f70256b33aed8003c9dabc529df
keskiju <jukka.keskiluopa@taitounited.fi>
2024-04-09 12:17:02 +0300

    feat: upgrade postgres client to v16

--- tools/install-db-clients.sh
+++ tools/install-db-clients.sh
@@ -4,6 +4,6 @@ set -eux; \
     curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add - && \
     echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list && \
     apt-get -qqy update && \
-    apt-get -qqy install postgresql-client-14 default-mysql-client && \
+    apt-get -qqy install postgresql-client-16 default-mysql-client && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
