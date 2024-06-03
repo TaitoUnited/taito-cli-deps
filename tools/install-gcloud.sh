f33654d41f4d02000c5ea03a56d61c03fad06d08
keskiju <jukka.keskiluopa@taitounited.fi>
2024-06-03 13:21:02 +0300

    fix: google-cloud-sdk was replaced with google-cloud-cli

--- tools/install-gcloud.sh
+++ tools/install-gcloud.sh
@@ -7,7 +7,7 @@ set -eux; \
       tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |
       apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && \
-    export GCLOUD_SDK_VERSION=${GCLOUD_SDK_VERSION:-478.0.0-0} && \
+    export GCLOUD_CLI_VERSION=${GCLOUD_CLI_VERSION:-478.0.0-0} && \
     export GCLOUD_AUTH_VERSION=${GCLOUD_AUTH_VERSION:-478.0.0-0} && \
-    apt-get update -y && apt-get install google-cloud-sdk=${GCLOUD_SDK_VERSION} -y && \
+    apt-get update -y && apt-get install google-cloud-cli=${GCLOUD_CLI_VERSION} -y && \
     apt-get update -y && apt-get install google-cloud-cli-gke-gcloud-auth-plugin=${GCLOUD_AUTH_VERSION} -y
