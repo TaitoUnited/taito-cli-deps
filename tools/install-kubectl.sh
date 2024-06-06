c004e13b5aaa5361c6193cfa3273ef63e6d2590c
keskiju <jukka.keskiluopa@taitounited.fi>
2024-06-06 20:32:50 +0300

    feat: kubectl 1.26

--- tools/install-kubectl.sh
+++ tools/install-kubectl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -eux; \
-    export KUBECTL_BASE_VERSION=${KUBECTL_BASE_VERSION:-1.28} && \
+    export KUBECTL_BASE_VERSION=${KUBECTL_BASE_VERSION:-1.26} && \
     curl -fsSL https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg && \
     echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBECTL_BASE_VERSION}/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list && \
     apt-get -qqy update && \
