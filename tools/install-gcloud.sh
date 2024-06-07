2ac06f43a76ea565ac8434509eaa9772caea532c
keskiju <jukka.keskiluopa@taitounited.fi>
2024-06-07 09:36:57 +0300

    chore: use gpg dearmor on gcloud install

--- tools/install-gcloud.sh
+++ tools/install-gcloud.sh
@@ -6,7 +6,7 @@ set -eux; \
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" |
       tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |
-      apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && \
+      gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && \
     export GCLOUD_CLI_VERSION=${GCLOUD_SDK_VERSION:-420.0.0-0} && \
     export GCLOUD_AUTH_VERSION=${GCLOUD_AUTH_VERSION:-420.0.0-0} && \
     apt-get update -y && apt-get install google-cloud-sdk=${GCLOUD_CLI_VERSION} -y && \
