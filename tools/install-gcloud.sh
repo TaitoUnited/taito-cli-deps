41edcd1e869afd056f98c49600ebf69afe9fe51b
keskiju <jukka.keskiluopa@taitounited.fi>
2024-06-07 10:00:17 +0300

    chore: add note on newer gcloud versions

--- tools/install-gcloud.sh
+++ tools/install-gcloud.sh
@@ -7,6 +7,7 @@ set -eux; \
       tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg |
       gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && \
+    # NOTE: There are some connection problems with newer gcloud versions: https://www.reddit.com/r/googlecloud/comments/1aopr51/gcloud_constantly_hangs_with_ipv6/
     export GCLOUD_CLI_VERSION=${GCLOUD_SDK_VERSION:-457.0.0-0} && \
     export GCLOUD_AUTH_VERSION=${GCLOUD_AUTH_VERSION:-457.0.0-0} && \
     apt-get update -y && apt-get install google-cloud-sdk=${GCLOUD_CLI_VERSION} -y && \
