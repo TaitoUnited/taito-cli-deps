8b6128a1fd379dc438eb5c8b8921992f0020e25a
keskiju <jukka.keskiluopa@taitounited.fi>
2024-09-07 08:37:58 +0300

    feat: update docker

--- tools/install-dockerd-bin.sh
+++ tools/install-dockerd-bin.sh
@@ -4,7 +4,7 @@ set -eux; \
     apt-get -qqy update && \
     apt-get -qqy install iptables && \
 
-    export DOCKER_VERSION=${DOCKER_VERSION:-26.1.3} && \
+    export DOCKER_VERSION=${DOCKER_VERSION:-27.2.0} && \
     export DOCKER_ARCH="x86_64" && \
     if [ ${TARGETPLATFORM} = "linux/arm64" ]; then export DOCKER_ARCH="aarch64"; fi && \
     curl -fsSL \
