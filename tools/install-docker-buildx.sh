d49a37e11a006a291c13aaeac1552488ab3121ac
keskiju <jukka.keskiluopa@taitounited.fi>
2024-11-23 19:46:47 +0200

    feat: install docker buildx

---
+++ tools/install-docker-buildx.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+set -eux; \
+    export DOCKER_BUILDX_VERSION=${DOCKER_BUILDX_VERSION:-0.18.0} && \
+    export DOCKER_BUILDX_ARCH="linux-amd64" && \
+    if [ ${TARGETPLATFORM} = "linux/arm64" ]; then export DOCKER_BUILDX_ARCH="linux-arm64"; fi && \
+    mkdir -p /usr/local/lib/docker/cli-plugins && \
+    curl -SL "https://github.com/docker/buildx/releases/download/v$DOCKER_BUILDX_VERSION/buildx-v$DOCKER_BUILDX_VERSION.$DOCKER_BUILDX_ARCH" -o /usr/local/lib/docker/cli-plugins/docker-buildx && \
+    chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
