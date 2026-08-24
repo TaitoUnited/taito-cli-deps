1ab1703023e90c4f6e5489baa6f56727a2c35556
keskiju <jukka.keskiluopa@taitounited.fi>
2026-08-24 15:57:30 +0300

    fix: remove upx from docker for now

--- tools/install-docker-bin.sh
+++ tools/install-docker-bin.sh
@@ -9,7 +9,7 @@ set -eux; \
     curl -fsSL \
       https://download.docker.com/linux/static/stable/${DOCKER_ARCH}/docker-$DOCKER_VERSION.tgz | \
       tar -xzvf - docker/docker && \
-    upx -9v docker/docker && \
+    # upx -9v docker/docker && \
     mv docker/docker /usr/bin/docker && \
     rmdir docker && \
     groupadd docker && \
