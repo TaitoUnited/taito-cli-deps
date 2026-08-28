7f35293a252987c47e5dfde2c4301002ea89ab94
keskiju <jukka.keskiluopa@taitounited.fi>
2026-08-28 21:14:37 +0300

    chore: enable upx

--- tools/install-docker-buildx.sh
+++ tools/install-docker-buildx.sh
@@ -6,5 +6,5 @@ set -eux; \
     if [ ${TARGETPLATFORM} = "linux/arm64" ]; then export DOCKER_BUILDX_ARCH="linux-arm64"; fi && \
     mkdir -p /usr/local/lib/docker/cli-plugins && \
     curl -SL "https://github.com/docker/buildx/releases/download/v$DOCKER_BUILDX_VERSION/buildx-v$DOCKER_BUILDX_VERSION.$DOCKER_BUILDX_ARCH" -o /usr/local/lib/docker/cli-plugins/docker-buildx && \
-    chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx
-    # upx -9v /usr/local/lib/docker/cli-plugins/docker-buildx
+    chmod +x /usr/local/lib/docker/cli-plugins/docker-buildx && \
+    upx -9v /usr/local/lib/docker/cli-plugins/docker-buildx
